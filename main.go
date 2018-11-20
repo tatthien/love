b502381cfa035c414b1404029ea8a2a6d994fa50
Thien Nguyen <tatthien.contact@gmail.com>
2018-11-20 17:34:19 +0700

    Get query string
    Replace emoji
    
    Fix path of template.txt
    
    Fix path of template.txt
    
    Copy template.txt to functions directory
    
    Fix template path
    
    Test

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"io/ioutil"
+	"strings"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	text := request.QueryStringParameters["text"]
+	data, err := ioutil.ReadFile("template.txt")
+
+	newText := strings.Replace(string(data), ":nhin_cai_gi:", text, -1)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body: "love",
+		Body: newText,
 	}, nil
 }
 
