4e316e67c76210bedf2ac63268cdf1d61c02afbf
Thien Nguyen <tatthien.contact@gmail.com>
2018-11-21 22:52:19 +0700

    Make the response text is visible to the channel

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"strings"
@@ -17,9 +18,18 @@ var heart = `:x::x::x::x::x::x::x::x::x::x::x:
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	emoji := request.QueryStringParameters["text"]
 	newHeart := strings.Replace(heart, ":x:", emoji, -1)
+
+	responseBody, _ := json.Marshal(map[string]string {
+		"response_type": "in_channel",
+		"text": newHeart,
+	})
+
 	return events.APIGatewayProxyResponse{
+		Headers: map[string]string {
+			"content-type": "application/json",
+		},
 		StatusCode: 200,
-		Body:       newHeart,
+		Body:       string(responseBody),
 	}, nil
 }
 
